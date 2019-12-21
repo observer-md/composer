eefe275f3af4daf57542d89b91b7d70f1ee9ee70
Batman <batman@example.com>
2019-12-20 21:45:20 -0500

    added query builder

---
+++ querybuilder/src/QueryBuilder.php
@@ -0,0 +1,15 @@
+<?php
+namespace ObserverMd\QueryBuilder;
+
+class QueryBuilder
+{
+    public function __construct()
+    {
+        var_dump(__METHOD__);
+    }
+
+    public function show()
+    {
+        var_dump(__METHOD__);
+    }
+}
